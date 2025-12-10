c56fc3ab32d9dabcbe676521215a620b5ac3876e
Maelys <joret.maelys@gmail.com>
2025-12-10 15:15:28 +0100

    Initial commit Co-authored-by: Cassandre9610 <cassandre.yontailleux@gmail.com>

---
+++ src/db/schema.js
@@ -0,0 +1,2 @@
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { randomUUID } from "crypto";
---
+++ src/db/seed.js
@@ -0,0 +1,2 @@
+import { db } from "./database.js";
+import bcrypt from "bcrypt";
